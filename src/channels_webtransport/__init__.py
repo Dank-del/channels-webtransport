6ff78593b8b707e376e8cd88a94c8365cc6094d4
Sayan Biswas <sayan.biswas@youe.co>
2026-08-15 21:18:35 +0530

    Set initial package version to 0.0.1

--- src/channels_webtransport/__init__.py
+++ src/channels_webtransport/__init__.py
@@ -4,4 +4,4 @@ from .adapter import WebTransportASGIServer
 from .consumer import AsyncWebTransportConsumer
 
 __all__ = ["AsyncWebTransportConsumer", "WebTransportASGIServer"]
-__version__ = "0.1.0"
+__version__ = "0.0.1"
